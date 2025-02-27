42f2fc474d766657cfa7a5044f9629f2005f9e78
Martin <martin.ng@live.com>
2025-02-27 15:57:52 +0100

    Initialise express

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
